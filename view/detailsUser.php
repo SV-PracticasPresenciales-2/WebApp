90beefb30222c6eb2d45da7c02a98d4de5a5564b
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-05-13 17:06:46 +0200

    Page style updated

--- view/detailsUser.php
+++ view/detailsUser.php
@@ -4,36 +4,70 @@ include_once '../controller/userController.php';
 include_once '../controller/postUserController.php';
 
 ?>
-<div class="container-sm text-center justify-content-center w-25 mt-5">
-    <div class="row justify-content-center mb-1">
-        <div class="col">
-            Username:<?php echo $user->getUsername(); ?></td>
+<div class="container column justify-content-center mb-1">
+    <div class="container-sm text-center justify-content-center w-25 mt-5">
+        <div class="row justify-content-center mb-1">
+            <h2>
+                User Details:
+            </h2>
+            <table id="mytable" class="table table-bordred table-striped">
+
+                <thead>
+
+                <th>Username</th>
+                <th>Biography</th>
+
+                </thead>
+                <tbody>
+                        <tr>
+                            <td><?php echo $user->getUsername(); ?></td>
+                            <td><?php echo $user->getBiography(); ?></td>
+                        </tr>
+                </tbody>
+
+            </table>
         </div>
-        <div class="col">
-            Biography:<?php echo $user->getBiography(); ?></td>
-        </div>
+
     </div>
 
-</div>
+        <div class="row">
 
-<div class="container-sm text-center justify-content-center w-25 mt-5">
-    <?php
-    if ($posts != null) {
-    foreach ($posts as $post): ?>
-    <div class="row justify-content-center mb-1">
 
-        <div class="col">
-            Message:<?php echo $post['message'] ?></td>
+            <div class="col-md-12">
+                <div class="table-responsive">
+
+
+                    <table id="mytable" class="table table-bordred table-striped">
+
+                        <thead>
+
+                        <th>Message</th>
+                        <th>Likes</th>
+                        <th>Post date</th>
+
+                        </thead>
+                        <tbody>
+
+                        <?php
+                        if ($posts != null) {
+                        foreach ($posts as $post): ?>
+                            <tr>
+                                <td><?php echo $post['message'] ?></td>
+                                <td><?php echo $post['likes'] ?></td>
+                                <td><?php echo $post['post_date'] ?></td>
+                            </tr>
+                        <?php endforeach;
+                        }
+                        ?>
+                        </tbody>
+
+                    </table>
+
+                    <div class="clearfix"></div>
+                </div>
+
+            </div>
         </div>
-    </div>
-    <?php endforeach;
-    } else {
-    ?>
-    <div class="row justify-content-center mb-1">
-        This user hast posted yet
-    </div
-    <?php } ?>
 </div>
-
 </body>
 </html>
--- view/listPosts.php
+++ view/listPosts.php
@@ -2,33 +2,47 @@
 include("header.php");
 include_once '../controller/postController.php';
 ?>
-<div class="container-sm text-center justify-content-center w-25 mt-5">
-    <div class="row justify-content-center mb-1">
-        <h3>List posts
-    </div>
-
-</div>
-<div class="container-sm text-center justify-content-center w-25 mt-5">
-    <?php
-    foreach ($posts
-             as
-             $post): ?>
-        <div class="row justify-content-center mb-1">
+<div class="container">
+    <div class="row">
 
 
-            <div class="row justify-content-center mb-1">
-                <div class="col">
-                    Username:<?php echo $userController->getName($post['post_user'])['username'] ?></td>
-                </div>
-                <div class="col">
-                    Mensaje:<?php echo $post['message'] ?></td>
-                </div>
-                <div class="col">
-                    <a href="detailsUser.php?id=<?php echo $post['post_user']?>" class="btn btn-info" type="button">User Details</a>
-                </div>
+        <div class="col-md-12">
+            <h4>Global Feed Posts</h4>
+            <div class="table-responsive">
+
+
+                <table id="mytable" class="table table-bordred table-striped">
+
+                    <thead>
+
+                    <th>Username</th>
+                    <th>Message</th>
+                    <th>Post date</th>
+                    <th>Details</th>
+
+                    </thead>
+                    <tbody>
+
+                    <?php
+                    foreach ($posts
+                    as
+                    $post): ?>
+                    <tr>
+                        <td><?php echo $userController->getName($post['post_user'])['username'] ?></td>
+                        <td><?php echo $post['message'] ?></td>
+                        <td><?php echo $post['post_date'] ?></td>
+                        <td><p data-placement="top" data-toggle="tooltip" title="See details"><a href="detailsUser.php?id=<?php echo $post['post_user']?>" class="btn btn-info" type="button">User Details</a></p></td>
+                    </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+
+                </table>
+
+                <div class="clearfix"></div>
             </div>
+
         </div>
-    <?php endforeach; ?>
+    </div>
 </div>
 </body>
 </html>
